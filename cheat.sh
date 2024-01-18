d2c6f09584b2f51349d7e6370dfaf851f100c37d
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-01-18 13:44:03 -0600

    Cleanup fzf script

--- cheat.sh
+++ cheat.sh
@@ -7,45 +7,19 @@ if ! [ -e ~/.config/tmux/cht-utils ]; then
   echo "mv|sed|awk|git|ps|ssh|cargo|tr|jq|docker|docker-compose" | tr '|' '\n' > ~/.config/tmux/cht-utils
 fi
 
-get_fzf_options() {
-    local cht_options
-    local cht_default_options='-d 35% -m -0 --no-preview --no-border'
-    cht_options="$(tmux show -gqv '@cheat-sh-options')"
-    [ -n "$cht_options" ] && echo "$cht_options" || echo "$cht_default_options"
-}
-
-fzf_filter() {
-  eval "fzf-tmux $(get_fzf_options)"
-}
-
-limit='screen'
-[[ $# -ge 2 ]] && limit=$2
-
-if [[ $limit == 'screen' ]]; then
-    content="$(tmux capture-pane -J -p)"
-else
-    content="$(tmux capture-pane -J -p -S -"$limit")"
-fi
-
-if [[ $# -ge 1 && "$1" != '' ]]; then
-    extras=$(echo "$content" |eval "$1")
-fi
-
 open_info() {
-  local query
-  read -p "Enter Query: " query
-
-  query=`echo $query | tr ' ' '+'`
-  tmux neww bash -c "echo \"cht.sh $1/$query\" & cht.sh $1/$query; echo \"Press any key to continue\"; read -rsn1"
+  local query; \
+    read -p "$(printf "Enter query for %s: " "$1")" query; \
+  query=`echo $query | tr ' ' '+'`; \
+  tmux neww bash -c "echo \"cht.sh $1/$query\" & cht.sh $1/$query; echo \"Press any key to continue\"; read -rsn1"; \
   exit 0
 }
 
 items="$(printf '%s|%s' $(cat ~/.config/tmux/cht-langs | tr '\n' '|') $(cat ~/.config/tmux/cht-utils | tr '\n' '|') |
-    grep -v '^$' |
     tr '|' '\n' |
-    sort -u |
-    nl -w3 -s '  '
+    grep -v '^$' |
+    sort -u
 )"
 [ -z "$items" ] && tmux display 'cht-sh: No languages or utilities found.' && exit
 
-open_info "$(fzf_filter <<< "$items" | awk '{print $2}')"
+open_info "$(echo "$items" | fzf)"
